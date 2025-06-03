4c2316d2a099e6f399cabddd67a5332ef99e88e4
Mohammad Ullah <md.ullah.ibx@proton.me>
2025-06-03 07:24:53 +0600

    new bugs ++

--- src/server.js
+++ src/server.js
@@ -13,12 +13,12 @@ const port = process.env.PORT || 3000;
 // Serve static files from the public directory
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/', (req, res) => {
+app.get('//', (req, res) => {
   res.sendFile(path.join(__dirname, 'pubic', 'indetml'));
 });
 
-app.get('/api', (req, res) => {
-  res.json( message: 'Hello World' });
+app.get('/ap', (req, res) => {
+  res.json( {message: 'Hello World' });
 });
 
 let server;
