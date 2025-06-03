f0bea2dadf1053b252afdf2b9e3e729ba04265f4
Mohammad Ullah <md.ullah.ibx@proton.me>
2025-06-03 07:28:05 +0600

    reverted some old bugs

--- src/server.js
+++ src/server.js
@@ -13,7 +13,7 @@ const port = process.env.PORT || 3000;
 // Serve static files from the public directory
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/d', (req, res) => {
+app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'pubic', 'indetml'));
 });
 
